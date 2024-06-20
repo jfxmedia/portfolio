e51f00126660387c0b556a0adb89077e0e26d2f5
Jesse Foster <jesf777@gmail.com>
2024-06-19 20:35:38 -0400

    test

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,9 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   base: '/portfolio/', 
+  build: {
+    outDir: 'build', // Ensure this is where your output is expected
+  },
   plugins: [react()],
   server: {
     port: 3000
