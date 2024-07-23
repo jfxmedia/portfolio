88c84b5623320f23aeca940fa0412b523a15d49f
Jesse Foster <jesf777@gmail.com>
2024-07-23 12:21:43 -0400

    removed hi component

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,7 +1,6 @@
 /* eslint-disable react/prop-types */
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faChevronDown } from '@fortawesome/free-solid-svg-icons'; 
-import SayHi from './SayHi';
 
 const Hero = ({ title, subtitle, scrollToRef }) => {
   const handleClick = () => {
@@ -16,7 +15,6 @@ const Hero = ({ title, subtitle, scrollToRef }) => {
             <div className="text-3xl p-10 font-bold text-cyan-700">{subtitle}</div>
             <h1 className="font-bold tracking-tight text-left lg:text-center text-transparent bg-gradient-to-r from-fuchsia-400 to-cyan-400 text-7xl md:text-8xl lg:text-9xl bg-clip-text">{title}</h1>
           </div>
-            <SayHi />
           <FontAwesomeIcon 
             onClick={handleClick} 
             className=' text-cyan-700 text-4xl mx-auto animate-bounceLimited pb-16 cursor-pointer' 
